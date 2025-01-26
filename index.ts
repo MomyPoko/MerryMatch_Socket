13a1a8341eb422347e836c3e921d7ab426343ff8
Chavanvit <chavanvit.n@gmail.com>
2025-01-26 10:43:06 +0700

    fix: index

--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://merry-matching.vercel.app/",
+    origin: "process.env.FRONTEND_URL",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
     credentials: true,
